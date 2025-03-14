9a678e670df0e92804339c60bb73b3d1a3d07526
NabilMita <30633006+NabilMita@users.noreply.github.com>
2025-03-14 13:06:07 +0100

    Create Oxygen Error Page.js

---
+++ Oxygen Error Page.js
@@ -0,0 +1,96 @@
+<!-- 
+Description : Home page d'oxygen
+***************************************************
+Créée par Roni Alain SONMEZ, le 29/04/2024 - 14h00 
+***************************************************
+Historique de modification : 
++ Modifiée par Xxxx XXXX, le XX/XX/XXXX - 00H00
++ Modifiée par Xxxx XXXX, le XX/XX/XXXX - 00H00
++ Modifiée par Xxxx XXXX, le XX/XX/XXXX - 00H00
+-->
+%%[
+    VAR @BU
+    SET @BU = upperCase(RequestParameter('BU'))
+    
+
+]%% 
+<!doctype html>
+<html lang="fr">
+ <head itemscope itemtype="http://schema.org/Article">
+  <meta name="keywords" content="">
+  <meta charset="utf-8">
+  <title>Oxygen | Une solution de la MOE</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no,date=no,address=no,email=no,url=no">
+  <meta name="theme-color" content="#25465f">
+  <meta name="description" content="Oxygen | Une solution de la MOE">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="referrer" content="no-referrer">
+  <link rel="canonical" href="https://www.bouyguestelecom.fr">
+  <meta property="og:title" content="Oxygen | Une solution de la MOE">
+  <meta property="og:description" content="Bienvenue dans Oxygen">
+  <meta property="og:type" content="article">
+  <meta property="og:url" content="https://www.bouyguestelecom.fr">
+  <meta property="og:image" itemprop="image" content="https://www.bouyguestelecom.fr/static/logo-bouygues-telecom.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="stylesheet" type="text/css" href="https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.8.8/trilogy.css">
+        <style type="text/css">.sf-container{margin-top: -60px !important}.sf-titre{padding-top:1rem !important;margin-bottom: 10px !important;}article{flex:1}@keyframes dontBeShy{from{opacity:0}to{opacity:1}}.animation-first,.animation-second,.animation-third{opacity:0}.animation-first{animation:.9s ease-in-out .5s forwards dontBeShy}.animation-second{animation:1.1s ease-in-out 1.3s forwards dontBeShy}.animation-third{animation:1.3s ease-in-out 2.2s forwards dontBeShy}</style>
+ </head>
+ <body class="has-background-grey-light">
+        <section class="hero is-info is-overlapped is-hidden-mobile" style="background: url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/jpg/transparent-noise.png),url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/svg/pattern-dark-logo.svg) no-repeat,linear-gradient(#1b3650, #0C7B91);background-size:auto,90%;background-position:0 0,333px 33%,0 0;height: 250px">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="headline is-level-1 has-text-white is-uppercase sf-titre">Accès non autorisé</h1>
+                </div>
+            </div>
+        </section>
+        <section class="hero is-overlapped is-medium is-hidden-tablet" style="background: url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/jpg/transparent-noise.png),url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/svg/pattern-dark-logo.svg) no-repeat,linear-gradient(#1b3650, #0C7B91);background-size:auto,125%;background-position:left 50%">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="headline is-level-1 has-text-white is-uppercase sf-titre">Accès non autorisé</h1>
+                </div>
+            </div>
+        </section>
+        <article>
+           <section class="section has-background-grey-light">
+                <div class="container sf-container">
+                    <div class="columns is-centered">
+                        <div class="column animation-first is-7">
+                            <div class="box is-loaded has-text-centered">
+                                <div class="box-content has-text-centered">
+                                    <span class="icon is-huge is-error"><i class="tri-exclamation-circle is-info" aria-hidden="true"></i></span>
+                                    <p class="is-size-5 has-text-centered has-text-tertiary">
+                                        Vous ne disposez pas d'autorisations nécessaires pour afficher le menu pour le profil %%=v(@BU)=%%.
+                                    </p>
+                                    <p class="is-size-5 has-text-centered has-text-tertiary">
+                                        Pour avoir accès à cette rubrique, merci d'en faire la demande auprès de votre administrateur Marketing Cloud.
+                                    </p>
+                                    <div class="buttons">
+                                        <a class="button is-loaded is-secondary" href="https://site.infos.bouyguestelecom.fr/oxygen-context">Retour Home Page</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </article>
+        <footer>
+            <section class="section has-background-grey-light">
+                <div class="container has-text-centered" style="background-image: url(https://image.infos.bouyguestelecom.fr/lib/fe8e13727467027470/m/1/dee77e38-85c2-4181-9c42-0f3cf4c8e69c.png); background-repeat: no-repeat; height: 50px; background-position: center; background-size: contain;"></div>
+            </section>
+        </footer>
+ </body>
+</html>
