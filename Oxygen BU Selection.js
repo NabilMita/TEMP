1bb74acd819b6ca4fe66ca8c08861e5a95ce1072
NabilMita <30633006+NabilMita@users.noreply.github.com>
2025-03-14 13:06:34 +0100

    Create Oxygen BU Selection.js

---
+++ Oxygen BU Selection.js
@@ -0,0 +1,128 @@
+<!-- 
+Description : Home page d'oxygen
+***************************************************
+Créée par Roni Alain SONMEZ, le 23/04/2024 - 14h00 
+***************************************************
+Historique de modification : 
++ Modifiée par Xxxx XXXX, le XX/XX/XXXX - 00H00
++ Modifiée par Xxxx XXXX, le XX/XX/XXXX - 00H00
++ Modifiée par Xxxx XXXX, le XX/XX/XXXX - 00H00
+-->
+%%[
+SET @_REDIRECT_ADMIN = CloudPagesURL(1724, 'BU', 'admin')
+SET @_REDIRECT_MARKETING = CloudPagesURL(1724, 'BU', 'marketing')
+SET @_REDIRECT_PROSPECT = CloudPagesURL(1724, 'BU', 'prospect')
+]%%
+<!doctype html>
+<html lang="fr">
+ <head itemscope itemtype="http://schema.org/Article">
+  <meta name="keywords" content="">
+  <meta charset="utf-8">
+  <title>Oxygen | Une solution de la MOE</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no,date=no,address=no,email=no,url=no">
+  <meta name="theme-color" content="#25465f">
+  <meta name="description" content="Oxygen | Une solution de la MOE">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="referrer" content="no-referrer">
+  <link rel="canonical" href="https://www.bouyguestelecom.fr">
+  <meta property="og:title" content="Oxygen | Une solution de la MOE">
+  <meta property="og:description" content="Bienvenue dans Oxygen">
+  <meta property="og:type" content="article">
+  <meta property="og:url" content="https://www.bouyguestelecom.fr">
+  <meta property="og:image" itemprop="image" content="https://www.bouyguestelecom.fr/static/logo-bouygues-telecom.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="https://www.bouyguestelecom.fr/skin/frontend/bytel/default/medias/images/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="stylesheet" type="text/css" href="https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.8.8/trilogy.css">
+        <style type="text/css">.sf-container{margin-top: -60px !important}.sf-titre{padding-top:1rem !important;margin-bottom: 10px !important;}.sf-ss-titre{padding-top:1rem !important;margin-bottom: 0px !important}.sf-card{width: 95% !important; margin: 0 auto !important;margin-bottom: 3rem !important}article{flex:1}@keyframes dontBeShy{from{opacity:0}to{opacity:1}}.animation-first,.animation-second,.animation-third{opacity:0}.animation-first{animation:.9s ease-in-out .5s forwards dontBeShy}.animation-second{animation:1.1s ease-in-out 1.3s forwards dontBeShy}.animation-third{animation:1.3s ease-in-out 2.2s forwards dontBeShy}</style>
+ </head>
+ <body class="has-background-grey-light">
+        <section class="hero is-info is-overlapped is-hidden-mobile" style="background: url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/jpg/transparent-noise.png),url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/svg/pattern-dark-logo.svg) no-repeat,linear-gradient(#1b3650, #0C7B91);background-size:auto,90%;background-position:0 0,333px 33%,0 0;height: 250px">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="headline is-level-1 has-text-white is-uppercase sf-titre">Bienvenue dans oxygen</h1>
+                    <h2 class="headline is-level-3 has-text-centered has-text-white ss-titre">Veuillez choisir votre profil</h2>
+                    <p class="is-size-4 has-text-centered has-text-white">Il faut s'assurer que vous avez les droits nécessaires pour le profil sélectionné</p>
+                </div>
+            </div>
+        </section>
+        <section class="hero is-overlapped is-medium is-hidden-tablet" style="background: url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/jpg/transparent-noise.png),url(https://assets.bouyguestelecom.fr/TRILOGY/trilogy-framework@3.5.1/assets/svg/pattern-dark-logo.svg) no-repeat,linear-gradient(#1b3650, #0C7B91);background-size:auto,125%;background-position:left 50%">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="headline is-level-1 has-text-white is-uppercase sf-titre">Bienvenue dans oxygen</h1>
+                    <h2 class="headline is-level-3 has-text-centered has-text-white ss-titre">Veuillez choisir votre profil</h2>
+                    <p class="is-size-4 has-text-centered has-text-white">Il faut s'assurer que vous avez les droits nécessaires pour le profil sélectionné</p>
+                </div>
+            </div>
+        </section>
+        <article>
+            <section class="section has-background-grey-light">
+                <div class="container sf-container">
+                    <div class="columns is-centered">
+                        <div class="column animation-first">
+                            <a href="%%=RedirectTo(@_REDIRECT_ADMIN)=%%">
+                                <div class="card sf-card">
+                                    <div class="card-image">
+                                        <figure class="image">
+                                            <img src="https://image.infos.bouyguestelecom.fr/lib/fe8e13727467027470/m/1/c415308d-1eed-4ff8-8177-a665cbe60715.jpg" alt="Admin">
+                                        </figure>
+                                    </div>
+                                    <div class="card-content">
+                                        <h2 class="headline is-level-3 has-text-centered"><span class="has-text-primary">A</span><span class="has-variant">d</span>min</h2>
+                                        <p class="is-size-5 has-text-centered has-text-tertiary">Réalisation et maintenance <br class="is-hidden-mobile">des solutions techniques</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="column animation-second">
+                            <a href="%%=RedirectTo(@_REDIRECT_MARKETING)=%%">
+                                <div class="card sf-card">
+                                    <div class="card-image">
+                                        <figure class="image">
+                                            <img src="https://image.infos.bouyguestelecom.fr/lib/fe8e13727467027470/m/1/cd2b7ba2-0b7f-41de-b612-09b440de148c.jpg" alt="Marketing">
+                                        </figure>
+                                    </div>
+                                    <div class="card-content">
+                                        <h2 class="headline is-level-3 has-text-centered"><span class="has-text-primary">M</span><span class="has-variant">a</span>rketing</h2>
+                                        <p class="is-size-5 has-text-centered has-text-tertiary">Création, gestion et ciblage <br class="is-hidden-mobile">des campagnes clients</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="column animation-third">
+                            <a href="%%=RedirectTo(@_REDIRECT_PROSPECT)=%%">
+                                <div class="card sf-card">
+                                    <div class="card-image">
+                                        <figure class="image">
+                                            <img src="https://image.infos.bouyguestelecom.fr/lib/fe8e13727467027470/m/1/9fe184a3-5032-4d0a-b171-65402be88d1d.jpg" alt="Prospect">
+                                        </figure>
+                                    </div>
+                                    <div class="card-content">
+                                        <h2 class="headline is-level-3 has-text-centered"><span class="has-text-primary">P</span>r<span class="has-variant">o</span>spect</h2>
+                                        <p class="is-size-5 has-text-centered has-text-tertiary">Création, gestion et ciblage <br class="is-hidden-mobile">des campagnes prospect</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </article>
+        <footer>
+            <section class="section has-background-grey-light">
+                <div class="container has-text-centered" style="background-image: url(https://image.infos.bouyguestelecom.fr/lib/fe8e13727467027470/m/1/dee77e38-85c2-4181-9c42-0f3cf4c8e69c.png); background-repeat: no-repeat; height: 50px; background-position: center; background-size: contain;"></div>
+            </section>
+        </footer>
+ </body>
+</html>
